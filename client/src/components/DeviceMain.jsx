8c10c3c8135e225b6162255e77a14e77ad7b291f
katya-sy <katya_sy_28@mail.ru>
2024-01-05 14:14:10 +0900

    add block with characteristics on deviceid page

--- client/src/components/DeviceMain.jsx
+++ client/src/components/DeviceMain.jsx
@@ -15,25 +15,43 @@ function DeviceMain() {
       "https://i.pinimg.com/236x/23/b9/e8/23b9e86b29aecf76cc46575303487018.jpg",
     ],
   };
+  const description = [
+    { id: 1, title: "Country", desc: "China" },
+    { id: 2, title: "Color", desc: "White" },
+    { id: 3, title: "Type", desc: "Vacuum cleaner" },
+  ];
 
   return (
     <div className="device-main">
-      <div className="device-main__images">
-        {device.img.map((img) => (
-          <img key={img} src={img} alt="Device" />
-        ))}
+      <div className="device-main__content">
+        <div className="device-main__images">
+          {device.img.map((img) => (
+            <img key={img} src={img} alt="Device" />
+          ))}
+        </div>
+        <div className="device-main__content-info">
+          <div>
+            <h2 className="device-main__title">{device.name}</h2>
+            <p className="device-main__desc">{device.desc}</p>
+            <p className="device-main__price">{device.price} ₽</p>
+          </div>
+          <div className="device-main__btns">
+            <Button style={{ width: "50%" }}>Купить</Button>
+            <button>
+              <img src={heart} />
+            </button>
+          </div>
+        </div>
       </div>
       <div className="device-main__info">
-        <div>
-          <h2 className="device-main__title">{device.name}</h2>
-          <p className="device-main__desc">{device.desc}</p>
-          <p className="device-main__price">{device.price} ₽</p>
-        </div>
-        <div className="device-main__btns">
-          <Button style={{ width: "50%" }}>Купить</Button>
-          <button>
-            <img src={heart} />
-          </button>
+        <h2 className="device-main__title">Характеристики</h2>
+        <div className="device-main__info-wrapper">
+          {description.map((desc) => (
+            <div key={desc.id} className="device-main__info-item">
+              <h4>{desc.title}</h4>
+              <p>{desc.desc}</p>
+            </div>
+          ))}
         </div>
       </div>
     </div>
