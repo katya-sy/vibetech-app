d9955c4d16d82c2ff57c76a36827dd3052b79f51
katya-sy <katya_sy_28@mail.ru>
2024-01-05 19:24:51 +0900

    login modal

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -7,6 +7,7 @@ import Footer from "./components/Footer";
 import Modal from "./components/UI/modal/Modal";
 import Form from "./components/UI/form/Form";
 import { Context } from "./main";
+import help from "./assets/img/help.svg";
 
 function App() {
   const [modalVisible, setModalVisible] = useState(false);
@@ -26,7 +27,15 @@ function App() {
       </div>
       <Footer />
       <Modal modalVisible={modalVisible} setModalVisible={setModalVisible}>
+        <h2 className="title form__title">Вход</h2>
         <Form buttonText={"Войти"} login={login} />
+        <div style={{ display: "flex", gap: 40 }}>
+          <button style={{ color: "var(--accent)" }}>Зарегистрироваться</button>
+          <button className="help">
+            <span>Нужна помощь?</span>
+            <img src={help} />
+          </button>
+        </div>
       </Modal>
     </BrowserRouter>
   );
